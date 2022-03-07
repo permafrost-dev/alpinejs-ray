0714ae4bc088e5382e5b2cfb04bf025538aa9fdc
Patrick Organ <patrick@permafrost.dev>
2022-03-07 18:22:57 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import AlpineRayPlugin from './AlpineRayMagicMethod';
 import { LibraryInitializer } from './lib/LibraryInitializer';
 import { getWindow } from './lib/utils';
 
-export { bootstrapImports } from './BootstrapImports';
-
 LibraryInitializer.create([AlpineRayPlugin], getWindow(), ray).init();
 
 export default AlpineRayPlugin;
