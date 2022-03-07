209b9f5e153e3b0edd727bed9f5d04919821f836
Patrick Organ <patrick@permafrost.dev>
2022-03-07 15:30:24 -0500

    wip

---
+++ src/PluginStandalone.ts
@@ -0,0 +1,8 @@
+import { getWindow } from './lib/utils';
+import pluginCallback from './Plugin';
+
+const win: any = getWindow();
+
+win.document.addEventListener('alpine:init', () => {
+    pluginCallback(win.Alpine, win);
+});
