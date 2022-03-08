7fb284af9ad0a41ecdbb8899f5309d57479a6671
Patrick Organ <patrick@permafrost.dev>
2022-03-08 03:19:04 -0500

    wip

--- tests/AlpineRay.test.ts
+++ tests/AlpineRay.test.ts
@@ -5,8 +5,3 @@ import { AlpineRay } from '../src/AlpineRay';
 it('has a version number', () => {
     expect(AlpineRay.$version).toBe('__BUILD_VERSION__');
 });
-
-it('returns an instance of the SpruceRay class', () => {
-    const ar = new AlpineRay();
-    expect(ar.spruce().constructor.name).toBe('SpruceRay');
-});
