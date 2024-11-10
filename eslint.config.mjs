c71df0f6b70c9e03eb8c41b1ec26c1eb9ad375e5
Patrick Organ <patrick@permafrost.dev>
2024-11-10 15:11:21 -0500

    upgrade eslint and config to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import globals from "globals";
+import { Linter } from 'eslint';
+
+/** @type {Linter.Config} */
+const config = [
+    {
+        files: ["src/**/*.js", "src/**/*.ts"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "script",
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: 'module',
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
+    {
+        plugins: [tsPlugin],
+        // extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended'],
+        rules: {
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-var-requires': 'off',
+            indent: ['error', 4, { SwitchCase: 1 }],
+            'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+            'no-useless-escape': 'off',
+        },
+    },
+];
+
+export default config;
